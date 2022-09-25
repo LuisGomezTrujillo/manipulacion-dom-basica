287edcc146318dc79fd648458adb7e290ca5a6ba
Luis GT <luisgomeztrujillo@gmail.com>
2022-09-25 16:58:03 -0500

    .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,17 @@
+const h1=document.querySelector('h1');
+const form=document.querySelector('#form');
+const input1=document.querySelector('#calculo1');
+const input2=document.querySelector('#calculo2');
+const btn=document.querySelector('#btnCalcular');
+const pResultado=document.querySelector('#resultado');
+
+btn.addEventListener('click', sumarInputValues)
+
+function sumarInputValues(event){
+    // console.log({event});
+    // event.preventDefault();
+    //console.log('Escuchando el evento de click');
+    //console.log(input1.value + input2.value);
+    const sumaInputs=input1.value+input2.value;
+    pResultado.innerText="Resultado: "+sumaInputs;
+}
